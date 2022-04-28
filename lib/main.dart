368ebae433cc949eaa1bfb6d8f36cf94525e28d1
FlutLab.io <>
2022-04-28 13:21:32 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        actions: [
+          Icon(Icons.search),
+          Icon(Icons.more_vert)
+        ],
+        elevation: 8,
+        backgroundColor: Colors.deepOrange,
+        shadowColor: Colors.deepOrange,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/pic1.jpg'),
+          Image.asset('images/pic2.jpg'),
+          Image.asset('images/pic3.jpg'),
+        ],
+      ),
+      drawer: Drawer(
+        // Agrega un ListView al drawer. Esto asegura que el usuario pueda desplazarse
+        // a través de las opciones en el Drawer si no hay suficiente espacio vertical
+        // para adaptarse a todo.
+        child: ListView(
+          // Importante: elimina cualquier padding del ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.face_sharp),
+              title: Text('persona 1'),
+              onTap: () {
+                // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.face_sharp),
+              title: Text('persona 2'),
+              onTap: () {
+                // // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.face_sharp),
+              title: Text('persona 3'),
+              onTap: () {
+                // // Actualiza el estado de la aplicación
+                // ...
+                // Luego cierra el drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
